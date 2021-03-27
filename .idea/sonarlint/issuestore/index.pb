3ff18dae6ba2863abc96ab139e8add976a1713fd
Ming Chen <ming.chen@vixverify.com>
2021-03-27 17:24:02 +1100

    git init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+T
+$src/com/ming/canberra/easy/Main.java,9/6/966de94e03781b6dc46637ed89a95569b7c52e88
---
+++ src/com/ming/canberra/easy/Main.java
@@ -0,0 +1,8 @@
+package com.ming.canberra.easy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello algoexpert world");
+    }
+}
