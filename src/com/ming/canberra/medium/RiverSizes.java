c37a808490682bce5426f2dae08a814008f2640a
Ming <ming.edentiti@gmail.com>
2021-03-27 19:34:31 +1100

    get ready for my first medium one

---
+++ src/com/ming/canberra/medium/RiverSizes.java
@@ -0,0 +1,16 @@
+package com.ming.canberra.medium;
+
+import java.util.List;
+
+public class RiverSizes {
+    public static void main(String[] args) {
+        System.out.println("hello algoexpert world");
+
+        RiverSizes.riverSizes(null);
+    }
+
+    public static List<Integer> riverSizes(int[][] matrix) {
+        // Write your code here.
+        return null;
+    }
+}
