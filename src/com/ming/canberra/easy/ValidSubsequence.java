0132ffc8cd9fc9dfd5282a67762f2fd8918cd9a1
Ming Chen <ming.chen@vixverify.com>
2021-03-27 17:41:47 +1100

    my first algoexpert easy one

--- src/com/ming/canberra/easy/ValidSubsequence.java
+++ src/com/ming/canberra/easy/ValidSubsequence.java
@@ -3,18 +3,25 @@ package com.ming.canberra.easy;
 import java.util.List;
 
 public class ValidSubsequence {
-
     public static void main(String[] args) {
         System.out.println("hello algoexpert world");
 
         ValidSubsequence.isValidSubsequence(null, null);
     }
 
-
-
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-        // Write your code here.
-
+        int iArray = 0;
+        int iSequence = 0;
+        while (iArray < array.size())
+        {
+            if (array.get(iArray) == sequence.get(iSequence)){
+                iSequence++;
+                if (iSequence == sequence.size()){
+                    return true;
+                }
+            }
+            iArray++;
+        }
 
         return false;
     }
