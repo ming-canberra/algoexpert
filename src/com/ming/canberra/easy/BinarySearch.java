10eae87e40e4aab9991005c91b7162a889cc0d34
Ming <ming.edentiti@gmail.com>
2021-03-29 00:22:14 +1100

    easy binary search impl

--- src/com/ming/canberra/easy/BinarySearch.java
+++ src/com/ming/canberra/easy/BinarySearch.java
@@ -10,7 +10,31 @@ public class BinarySearch {
     }
 
     public static int binarySearch(int[] array, int target) {
-        // Write your code here.
-        return -1;
+        int lo = 0;
+        int hi = array.length - 1;
+
+        while (hi - lo > 1){
+            int m = lo + (hi - lo)/2;
+
+            if (array[m] == target){
+                return m;
+            }
+            else if (array[m] > target){
+                hi = m - 1;
+            }
+            else{
+                lo = m + 1;
+            }
+        }
+
+        if (target == array[lo]){
+            return lo;
+        }
+        else if (target == array[hi]){
+            return hi;
+        }
+        else{
+            return -1;
+        }
     }
 }
