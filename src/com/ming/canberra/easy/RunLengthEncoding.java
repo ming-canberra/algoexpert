96cf85812df19981c73d61ca1ddca93a255b3f17
Ming <ming.edentiti@gmail.com>
2021-03-29 19:04:50 +1100

    easy one

--- src/com/ming/canberra/easy/RunLengthEncoding.java
+++ src/com/ming/canberra/easy/RunLengthEncoding.java
@@ -8,7 +8,42 @@ public class RunLengthEncoding {
     }
 
     public String runLengthEncoding(String string) {
-        // Write your code here.
-        return "";
+        char[] chars = string.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        char currentChar = 0;
+        for (int i = 0; i < chars.length; i++){
+            if (i == 0){
+                currentChar = chars[i];
+                count++;
+            }
+            else{
+                if (chars[i] == currentChar){
+                    count++;
+                }
+                else{
+                    // append
+                    append(sb, count, currentChar);
+                    count = 1;
+                    currentChar = chars[i];
+                }
+            }
+            if (i == chars.length - 1){
+                //append
+                append(sb, count, currentChar);
+            }
+        }
+        return sb.toString();
+    }
+
+    private void append(StringBuilder sb, int count, char currentChar){
+            int countOfNine = count / 9 ;
+            int remainder = count % 9;
+            for (int i = 1; i <= countOfNine; i++){
+                sb.append(9).append(currentChar);
+            }
+            if (remainder >0){
+                sb.append(remainder).append(currentChar);
+            }
     }
 }
