11e94f023721e46df6f982af71aeff3c0e50891d
Ming <ming.edentiti@gmail.com>
2021-04-05 17:54:11 +1000

    skeleton code

---
+++ src/com/ming/canberra/medium/MinMaxStackConstruction.java
@@ -0,0 +1,38 @@
+package com.ming.canberra.medium;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class MinMaxStackConstruction {
+    public static void main(String[] args) {
+        int aa[] = new int[]{1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3};
+        MinMaxStackConstruction StaircaseTraversal = new MinMaxStackConstruction();
+    }
+    // Feel free to add new properties and methods to the class.
+    static class MinMaxStack {
+        public int peek() {
+            // Write your code here.
+            return -1;
+        }
+
+        public int pop() {
+            // Write your code here.
+            return -1;
+        }
+
+        public void push(Integer number) {
+            // Write your code here.
+        }
+
+        public int getMin() {
+            // Write your code here.
+            return -1;
+        }
+
+        public int getMax() {
+            // Write your code here.
+            return -1;
+        }
+    }
+}
+
