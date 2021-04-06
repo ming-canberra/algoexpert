0da21676d552577372585c6a76c6d4c70dd9e04e
Ming <ming.edentiti@gmail.com>
2021-04-07 01:14:50 +1000

    did not make it. BST tree is a bitch

--- src/com/ming/canberra/medium/BSTConstruction.java
+++ src/com/ming/canberra/medium/BSTConstruction.java
@@ -4,7 +4,7 @@ public class BSTConstruction {
     public static void main(String[] args) {
         int aa[] = new int[]{2, 1, 1};
         BSTConstruction arrayOfProducts = new BSTConstruction();
-    }
+   }
 
     static class BST {
         public int value;
@@ -18,12 +18,46 @@ public class BSTConstruction {
         public BST insert(int value) {
             // Write your code here.
             // Do not edit the return statement of this method.
+            insertRecursively(this, value);
             return this;
         }
-
+        private void insertRecursively(BST bst, int value) {
+            if (value < bst.value){
+                if (bst.left == null){
+                    BST newOne = new BST(value);
+                    bst.left = newOne;
+                }
+                else{
+                    insertRecursively(bst.left, value);
+                }
+            }
+            else{
+                if (bst.right == null){
+                    BST newOne = new BST(value);
+                    bst.right = newOne;
+                }
+                else{
+                    insertRecursively(bst.right, value);
+                }
+            }
+        }
         public boolean contains(int value) {
             // Write your code here.
-            return false;
+            return containsRecursively(this, value);
+        }
+        private boolean containsRecursively(BST bst, int value){
+            if (bst == null){
+                return false;
+            }
+            else if (bst.value == value){
+                return true;
+            }
+            else if (value < bst.value){
+                return containsRecursively(bst.left, value);
+            }
+            else{
+                return containsRecursively(bst.right, value);
+            }
         }
 
         public BST remove(int value) {
