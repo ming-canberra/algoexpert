c2305d6fc8b3efd5fddae170370bdfa5bae1ab3a
Ming <ming.edentiti@gmail.com>
2021-04-09 10:29:23 +1000

    I did not work it out. Need to do it again

--- src/com/ming/canberra/medium/LevenshteinDistance.java
+++ src/com/ming/canberra/medium/LevenshteinDistance.java
@@ -3,12 +3,23 @@ package com.ming.canberra.medium;
 public class LevenshteinDistance {
     public static void main(String[] args) {
         int aa[] = new int[]{1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3};
-        System.out.println (LevenshteinDistance.levenshteinDistance("basdf", "sdf"));
+        System.out.println (LevenshteinDistance.levenshteinDistance("biting", "mitten"));
     }
 
-    public static int levenshteinDistance(String str1, String str2) {
-        // Write your code here.
-        return -1;
+    public static int levenshteinDistance(String original, String target) {
+        int[][] dp = new int[original.length() + 1][target.length() + 1];
+        for (int i = 1; i <= original.length(); i++){
+            for (int j = 1; j <= target.length(); j++){
+                if (original.charAt(i - 1) == target.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println("dp " + dp[original.length()][target.length()]);
+        return Math.max(target.length(), original.length()) - dp[original.length()][target.length()];
     }
 }
 
