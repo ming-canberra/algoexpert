ff963dcad83c3fe7cc806e6174fa5db840f97305
Ming <ming.edentiti@gmail.com>
2021-04-11 23:50:11 +1000

    just copied and pasted other's implementation

--- src/com/ming/canberra/medium/LevenshteinDistance.java
+++ src/com/ming/canberra/medium/LevenshteinDistance.java
@@ -8,18 +8,23 @@ public class LevenshteinDistance {
 
     public static int levenshteinDistance(String original, String target) {
         int[][] dp = new int[original.length() + 1][target.length() + 1];
+        for (int i = 0; i < dp.length; i++){
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < dp[0].length; j++){
+            dp[0][j] = j;
+        }
         for (int i = 1; i <= original.length(); i++){
             for (int j = 1; j <= target.length(); j++){
                 if (original.charAt(i - 1) == target.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
             }
         }
-        System.out.println("dp " + dp[original.length()][target.length()]);
-        return Math.max(target.length(), original.length()) - dp[original.length()][target.length()];
+        return dp[original.length()][target.length()];
     }
 }
 
