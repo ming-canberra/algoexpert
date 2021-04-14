05c540d0052ba22fe12090782f38ad7b7e5def8c
Ming <ming.edentiti@gmail.com>
2021-04-14 19:50:36 +1000

    did it before, and I remember it

--- src/com/ming/canberra/hard/FindLoop.java
+++ src/com/ming/canberra/hard/FindLoop.java
@@ -6,8 +6,27 @@ public class FindLoop {
     }
 
     public static LinkedList findLoop(LinkedList head) {
-        // Write your code here.
-        return null;
+        LinkedList nodeSingleSpeed = head;
+        LinkedList nodeDoubleSpeed = head;
+        while(true){
+            nodeSingleSpeed = nodeSingleSpeed.next;
+            nodeDoubleSpeed = nodeDoubleSpeed.next.next;
+            if (nodeSingleSpeed == nodeDoubleSpeed){
+                break;
+            }
+        }
+        LinkedList nodeThird = head;
+        if (nodeThird == nodeSingleSpeed){
+            return nodeThird;
+        }
+        while(true){
+            nodeSingleSpeed = nodeSingleSpeed.next;
+            nodeThird = nodeThird.next;
+            if (nodeSingleSpeed == nodeThird){
+                break;
+            }
+        }
+        return nodeThird;
     }
 
     static class LinkedList {
