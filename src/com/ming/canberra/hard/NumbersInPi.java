e35560fb6cd2ba8c6e95ef9f2eb71d1b3f9af623
Ming <ming.edentiti@gmail.com>
2021-04-17 19:37:28 +1000

    get ready for next one

---
+++ src/com/ming/canberra/hard/NumbersInPi.java
@@ -0,0 +1,13 @@
+package com.ming.canberra.hard;
+
+public class NumbersInPi {
+    public static void main(String[] args) {
+        int res = NumbersInPi.numbersInPi("f", new String[]{""});
+        System.out.println(res);
+    }
+
+    public static int numbersInPi(String pi, String[] numbers) {
+        // Write your code here.
+        return -1;
+    }
+}
