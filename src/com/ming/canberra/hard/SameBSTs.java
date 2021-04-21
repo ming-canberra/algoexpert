fbc13d049aa1c99f4afb6520e2c0140603fde606
Ming <ming.edentiti@gmail.com>
2021-04-21 13:51:04 +1000

    leave it for now

---
+++ src/com/ming/canberra/hard/SameBSTs.java
@@ -0,0 +1,17 @@
+package com.ming.canberra.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SameBSTs {
+    public static void main(String[] args) {
+        boolean result = SameBSTs.sameBsts(null, null);
+        System.out.println(result);
+    }
+    public static boolean sameBsts(List<Integer> arrayOne, List<Integer> arrayTwo) {
+        return true;
+    }
+
+
+}
+
