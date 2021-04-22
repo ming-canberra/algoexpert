7c850ab723591de3b05e4057d3b7ee2a26f5f535
Ming <ming.edentiti@gmail.com>
2021-04-22 16:49:47 +1000

    this is too hard

---
+++ src/com/ming/canberra/hard/MaxPathSumInBinaryTree.java
@@ -0,0 +1,20 @@
+package com.ming.canberra.hard;
+
+public class MaxPathSumInBinaryTree {
+    public static void main(String[] args) {
+        MaxPathSumInBinaryTree.maxPathSum(null);
+    }
+    private int _result = 0;
+    public static int maxPathSum(BinaryTree tree) {
+    }
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
+
